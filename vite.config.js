8b218a7f7a8b2495068474972c20ae2c09edfe7f
FireployOfficial <fireploy.official@gmail.com>
2025-07-02 23:35:38 -0500

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT),
+      host: true,
+      cors: true,
+      allowedHosts: [env.VITE_FIREPLOY_HOST],
+    },
+    preview: {
+      port: parseInt(env.VITE_PORT),
+      host: true,
+      cors: true,
+      allowedHosts: [env.VITE_FIREPLOY_HOST],
+    },
+  });
+};
